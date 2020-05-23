465287cb3aed6d6198225793d3a1d2628fffe3a1
Corey Miller <coreyMiller353@gmail.com>
2020-05-23 00:25:47 -0400

    restructures code

---
+++ index.js
@@ -0,0 +1,19 @@
+const Joi = require('joi');
+const express = require('express');
+const app = express();
+const genres = require('./routes/genres')
+
+app.use(express.json());
+
+
+app.use('/api/genres', genres)
+function validateGenre(genre) {
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+
+  return Joi.validate(genre, schema);
+}
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
